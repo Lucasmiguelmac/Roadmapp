0237e141f698b51710d22c755856a74f1f25bb6d
Lucas Miguel <lucasmiguelmac@gmail.com>
2020-04-27 17:31:18 -0300

    ordenar units en roadmap_detail

--- roadmaps/views.py
+++ roadmaps/views.py
@@ -47,7 +47,7 @@ class RoadmapDetailView(DetailView):
         qs = sorted(
             queryset_chain,
             key=lambda instance: instance.place,
-            reverse=True #Cambiar ésto según el órden que quiero
+            reverse=False #Cambiar ésto según el órden que quiero
         )
         self.count = len(qs)
         context['unit_list']= qs
